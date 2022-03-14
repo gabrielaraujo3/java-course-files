96ba11f9aed2d3352ba9bc2025305e86da58043f
gabrielaraujo3 <araujog388@gmail.com>
2022-03-14 17:06:11 -0300

    Manipulando strings

---
+++ ManipulandoStrings/src/ManipulandoStrings.java
@@ -0,0 +1,19 @@
+import java.util.Locale;
+
+public class ManipulandoStrings {
+
+    public static void main(String[] args) {
+        String x = "    Ola mundo, esse é o novo mundo"    ;
+        System.out.println(x.length());
+        System.out.println(x + "concatenada");
+        System.out.println(x.contains("novo"));
+        System.out.println(x.indexOf("mundo"));
+        System.out.println(x.lastIndexOf("mundo"));
+        System.out.println(x.toUpperCase());
+        System.out.println(x.toLowerCase());
+        System.out.println(x.trim());
+        System.out.println(x.substring(9));
+
+        System.out.println(x.equals("ola"));
+    }
+}
