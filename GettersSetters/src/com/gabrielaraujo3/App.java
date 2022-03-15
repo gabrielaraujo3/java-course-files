6d7ef31098f93d3339800a4689fbf9dd15438a93
gabrielaraujo3 <araujog388@gmail.com>
2022-03-15 07:51:15 -0300

    Arraylist

--- GettersSetters/src/com/gabrielaraujo3/App.java
+++ GettersSetters/src/com/gabrielaraujo3/App.java
@@ -1,21 +1,27 @@
 package com.gabrielaraujo3;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
 
     public static void main(String[] args) {
-        User[] users = new User[] {
-                new User("Gabriel", "Araujo"),
-                new User("Gabriel", "Araujo"),
-                new User("Gabriel", "Araujo"),
-        };
+        List<User> users = new ArrayList<>();
 
+        int i = 0;
+        while ( i < 10) {
+            User actual = new User("Nome " + i, "Sobrenome " + i);
+            users.add(actual);
+            i++;
+        }
 
-        //[x][x][x]
+        System.out.println(users.get(9).getFirstName());
+        System.out.println(users.get(9).getLastName());
 
+        User user11 = new User("Nome 11", "Sobrenome 11");
+        users.add(user11);
 
-
-        System.out.println(users[2].getFirstName());
-        System.out.println(users[2].getLastName());
+        System.out.println(users.get(10).getFirstName());
+        System.out.println(users.get(10).getLastName());
     }
-
 }
