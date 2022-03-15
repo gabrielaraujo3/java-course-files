d21aae32d34fdca81ddfb506e9026e2becd5fdfe
gabrielaraujo3 <araujog388@gmail.com>
2022-03-15 09:42:42 -0300

    forEach de objetos

---
+++ ForEach/src/Main.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // String[] => Fixo
+        // new ArrayList<String>() => Dinâmicos
+        List<String> names = new ArrayList<>();
+        // como adicionar elementos no array:
+        names.add("Nome 4");
+        names.add("Nome 1");
+        names.add("Nome 3");
+        names.add("Nome 2");
+
+        Collections.sort(names);
+
+        for(String name : names) {
+            System.out.println(name);
+        }
+
+    }
+}
